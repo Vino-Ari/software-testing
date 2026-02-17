77c60bf836d5c554ae2ccfcdd15062d1e35dc0f0
Vino-Ari <ivannovoselskij495@gmail.com>
2026-02-17 23:59:43 +0300

    Тестирование решателя линейных уравнений

---
+++ src/test/java/ru/ac/uniyar/testingcourse/LinearEquationSolverTest.java
@@ -0,0 +1,59 @@
+package ru.ac.uniyar.testingcourse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LinearEquationSolverTest {
+    /**
+     * Проверка, правильно ли решается линейное уравнение из натуральных чисел
+     * Дано: numberOne = 3.0, numberTwo = 9.0
+     * Ожидаемый результат: solve(numberOne, numberTwo) вернёт 3.0
+     */
+    @Test
+    void naturalNumbersCase() {
+        assertThat(LinearEquationSolver.solve(3.0, 9.0)).isEqualTo(3.0);
+    }
+
+    /**
+     * Проверка, правильно ли решается линейное уравнение из рациональных чисел
+     * Дано: numberOne = 0.5, numberTwo = 0.25
+     * Ожидаемый результат: solve(numberOne, numberTwo) вернёт 0.5
+     */
+    @Test
+    void realNumbersCase() {
+        assertThat(LinearEquationSolver.solve(0.5, 0.25)).isEqualTo(0.5);
+    }
+
+    /**
+     * Проверка, правильно ли решается линейное уравнение c отрицательным числом
+     * Дано: numberOne = -0.5, numberTwo = 0.25
+     * Ожидаемый результат: solve(numberOne, numberTwo) вернёт -0.5
+     */
+    @Test
+    void negativeNumbersCase() {
+        assertThat(LinearEquationSolver.solve(-0.5, 0.25)).isEqualTo(-0.5);
+    }
+
+    /**
+     * Проверяется, что если при решении линейного уравнения корнем является любое число, возникает исключение
+     * Дано: numberOne = 0.0, numberTwo = 0.0
+     * Ожидаемый результат: solve(numberOne, numberTwo) вернёт AnyNumberIsRootException
+     */
+    @Test
+    void AnyNumberIsRoot() {
+        assertThatThrownBy(() -> LinearEquationSolver.solve(0.0, 0.0)).isInstanceOf(LinearEquationSolver
+                .AnyNumberIsRootException.class);
+    }
+
+    /**
+     * Проверка, что при отсутствии корней выведется null
+     * Дано: numberOne = 0.0, numberTwo = 10.0
+     * Ожидаемый результат: solve(numberOne, numberTwo) вернёт null
+     */
+    @Test
+    void nullCase() {
+        assertThat(LinearEquationSolver.solve(0.0, 10.0)).isNull();
+    }
+}
